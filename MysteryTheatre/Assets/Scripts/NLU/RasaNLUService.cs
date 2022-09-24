9658537466c325d287bdffc58ad8d1293ee2cf61
Joey Fallone <j.fallone@ualberta.ca>
2022-09-24 15:48:46 -0600

    Add avatars

--- MysteryTheatre/Assets/Scripts/NLU/RasaNLUService.cs
+++ MysteryTheatre/Assets/Scripts/NLU/RasaNLUService.cs
@@ -11,9 +11,9 @@ namespace NLU
         
         public override string GetResponse(string prompt)
         {
-            var responseString = client.GetResponse("localhost:5005");
+            // var responseString = client.GetResponse("localhost:5005");
             // return $"Dummy response for prompt: {prompt}";
-            //var responseString = "hi";
+            var responseString = "hi";
             return responseString;
         }
         
